fe4bf894d8685abbf8a1e61eeff4bf6f89130e4f
tuan-cpu <tuan.lq184323@sis.hust.edu.vn>
2022-11-11 16:17:30 +0700

    initial set up

---
+++ client/src/utils/constants.js
@@ -0,0 +1,5 @@
+import abi from '../../../smart_contract/artifacts/contracts/Transactions.sol/Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x5c6E50FEAff4AD9bCE08adAadbDE5148aDF6aCc1';
+//goerli address: 0x5c6E50FEAff4AD9bCE08adAadbDE5148aDF6aCc1
